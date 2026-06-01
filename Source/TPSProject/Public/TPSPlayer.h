f091637bd7166b41492b54572ce2b42763cadee5
박형빈 <cavin1999@gmail.com>
2026-06-01 14:07:31 +0900

    feat : 에셋 추가 플레이어 스켈레탈 메시 추가 + 3인칭 카메라 생성

--- Source/TPSProject/Public/TPSPlayer.h
+++ Source/TPSProject/Public/TPSPlayer.h
@@ -25,4 +25,13 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// 스프링 암 컴포넌트 선언
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* springArmComp;
+	
+	// 카메라 컴포넌트 선언
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* cameraComp;
+	
 };
