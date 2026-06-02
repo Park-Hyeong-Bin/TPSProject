b11fac0121da6e62774e82cfb16beef61d9d97f8
박형빈 <cavin1999@gmail.com>
2026-06-02 11:59:18 +0900

    feat : bullet 컴포넌트 추가 + 그레네이트 런처 에셋 추가

--- Source/TPSProject/Private/Bullet.cpp
+++ Source/TPSProject/Private/Bullet.cpp
@@ -3,12 +3,48 @@
 
 #include "Bullet.h"
 
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
 
 // Sets default values
 ABullet::ABullet()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	//충돌체 등록
+	collisonComp = CreateDefaultSubobject<USphereComponent>(TEXT("Collison Component"));
+	//충돌 프로필 설정
+	collisonComp -> SetCollisionProfileName(TEXT("BlockAll"));
+	//충돌체 크기 설정
+	collisonComp -> SetSphereRadius(13.f);
+	//루트 컴포넌트로 등록
+	RootComponent = collisonComp;
+	
+	//외관 컴포넌트 등록
+	bodyMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body Mesh Component"));
+	//계층화
+	bodyMeshComp -> SetupAttachment(collisonComp);
+	//외관 컴포넌트 충돌은 off
+	bodyMeshComp -> SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	//외관 크기 설정
+	bodyMeshComp -> SetRelativeScale3D(FVector(0.25f));
+	
+	//발사체 컴포넌트 등록
+	movementComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Movement Component"));
+	//movement 컴포넌트가 갱신시킬 컴포넌트 지정
+	movementComp -> SetUpdatedComponent(collisonComp);
+	//초기 속도
+	movementComp -> InitialSpeed = 5000.f;
+	//최대 속도
+	movementComp -> MaxSpeed = 5000.f;
+	// 반동 여부
+	movementComp -> bShouldBounce = true;
+	//반동 값(탄성)
+	movementComp -> Bounciness = 0.3f;	
+	
+	
 }
 
 // Called when the game starts or when spawned
