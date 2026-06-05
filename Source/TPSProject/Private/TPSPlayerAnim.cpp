574fe3ed7281c18c9a39de9e233912750bb8fec6
박형빈 <cavin1999@gmail.com>
2026-06-05 18:52:06 +0900

    feat : 점프 시스템 구현 + 애니메이션 추가

--- Source/TPSProject/Private/TPSPlayerAnim.cpp
+++ Source/TPSProject/Private/TPSPlayerAnim.cpp
@@ -4,6 +4,7 @@
 #include "TPSPlayerAnim.h"
 
 #include "TPSPlayer.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 //매 프레임 자동 호출 - Tick 직접안해도 UE 자체가 호출
 void UTPSPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
@@ -14,12 +15,22 @@ void UTPSPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
 	APawn* pawn = TryGetPawnOwner();
 	if (pawn == nullptr) return;
 	
-	//ATPSPlayer 폰의 속도 측정
+	//ATPSPlayer 폰의 상태 추출을 하기 위한 캐스팅 (속도, 점프 중인지)
 	ATPSPlayer* player = Cast<ATPSPlayer>(pawn);
 	if (player)
 	{
+		// 속도가 있는 상태인지
 		speed = player->GetVelocity().Size();
+		
+		// 공중에 떠 있는 상태인지
+		UCharacterMovementComponent* movement = player->GetCharacterMovement();
+		if (movement)
+		{
+			isInair = movement->IsFalling();
+		}
 	}
+	
+	
 	
 	//디버그
 	if (GEngine)
